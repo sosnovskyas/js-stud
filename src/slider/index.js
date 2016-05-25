dfa8ae2928df19d185cfa6a8f4fe495ff21e0ce7
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-05-25 19:43:17 +0300

    changed slider object

--- src/slider/index.js
+++ src/slider/index.js
@@ -6,45 +6,62 @@ export default class Slider {
   constructor({
     elem
   }) {
-    this.elem = elem;
+    this.slider = this.makeSlider(elem);
+
+    this.slider.lineCoords = this.slider.line.getBoundingClientRect();
+
+    // this.sliderLineCoords = sliderLine.getBoundingClientRect();
+    // this.sliderThumbCoords = sliderThumb.getBoundingClientRect();
+
+    // events
+    this.slider.thumb.ondragstart = () => false;
+    this.mouseMoveHandler = this.mouseMove.bind(this);
+    document.addEventListener('mousedown', event => this.mouseDown(event));
+    document.addEventListener('mouseup', event => this.mouseUp(event));
+  }
+
+  makeSlider(elem){
     let sliderLine = document.createElement('div');
     let sliderThumb = document.createElement('div');
 
     sliderLine.classList.add('slider__line');
     sliderThumb.classList.add('slider__thumb');
     sliderLine.appendChild(sliderThumb);
-    this.elem.appendChild(sliderLine);
 
-    this.thumb = sliderThumb;
-    this.sliderLineCoords = sliderLine.getBoundingClientRect();
-    this.sliderThumbCoords = sliderThumb.getBoundingClientRect();
+    elem.appendChild(sliderLine);
 
-    this.mouseMoveHandler = this.mouseMove.bind(this);
-
-    document.addEventListener('mousedown', event => this.mouseDown(event));
-    document.addEventListener('mouseup', event => this.mouseUp(event));
+    return {
+      elem: elem,
+      thumb: sliderThumb,
+      line: sliderLine,
+      moveHandler: this.mouseMove.bind(this)
+    }
   }
 
   mouseDown(event) {
+    console.log('down');
+
     const thumb = event.target.closest('.slider__thumb');
 
     if (!thumb) return;
 
-    document.addEventListener('mousemove', this.mouseMoveHandler);
-    console.log('down');
+    // document.addEventListener('mousemove', this.mouseMoveHandler);
+    document.addEventListener('mousemove', this.slider.moveHandler);
   }
 
   mouseUp(event) {
-    document.removeEventListener('mousemove', this.mouseMoveHandler);
     console.log('up');
+    // document.removeEventListener('mousemove', this.mouseMoveHandler);
+    document.removeEventListener('mousemove', this.slider.moveHandler);
   }
 
   mouseMove(event) {
+    console.log('mouseMove');
+    /*
     if (((event.clientX - this.sliderLineCoords.left) > 0) &&((this.sliderLineCoords.right - this.sliderLineCoords.left) > event.clientX)) {
       this.thumb.style.left = event.clientX - this.sliderLineCoords.left + 'px';
     }
-    // console.log(this.thumb);
-    console.log('clientX:', event.clientX, 'coord:', this.sliderLineCoords.right);
+    */
   }
 
 }
