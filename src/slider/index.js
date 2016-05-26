691486e2aee45894a21218049cf3fd753c937a45
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-05-26 13:17:31 +0300

    move event finished

--- src/slider/index.js
+++ src/slider/index.js
@@ -4,23 +4,18 @@ import './slider.styl'
 
 export default class Slider {
   constructor({
-    elem
+    elem,
+    minValue = 0,
+    maxValue = 100,
   }) {
-    this.slider = this.makeSlider(elem);
+    this._slider = this.makeSlider(elem);
+    this._slider.lineCoords = this._slider.line.getBoundingClientRect();
 
-    this.slider.lineCoords = this.slider.line.getBoundingClientRect();
-
-    // this.sliderLineCoords = sliderLine.getBoundingClientRect();
-    // this.sliderThumbCoords = sliderThumb.getBoundingClientRect();
-
-    // events
-    this.slider.thumb.ondragstart = () => false;
-    this.mouseMoveHandler = this.mouseMove.bind(this);
     document.addEventListener('mousedown', event => this.mouseDown(event));
-    document.addEventListener('mouseup', event => this.mouseUp(event));
+    document.addEventListener('mouseup', event => this._mouseUp(event));
   }
 
-  makeSlider(elem){
+  makeSlider(elem) {
     let sliderLine = document.createElement('div');
     let sliderThumb = document.createElement('div');
 
@@ -28,13 +23,15 @@ export default class Slider {
     sliderThumb.classList.add('slider__thumb');
     sliderLine.appendChild(sliderThumb);
 
+    sliderThumb.ondragstart = () => false;
+
     elem.appendChild(sliderLine);
 
     return {
       elem: elem,
       thumb: sliderThumb,
       line: sliderLine,
-      moveHandler: this.mouseMove.bind(this)
+      moveHandler: this._mouseMove.bind(this)
     }
   }
 
@@ -44,24 +41,52 @@ export default class Slider {
     const thumb = event.target.closest('.slider__thumb');
 
     if (!thumb) return;
-
-    // document.addEventListener('mousemove', this.mouseMoveHandler);
-    document.addEventListener('mousemove', this.slider.moveHandler);
+    console.dir(thumb.getBoundingClientRect());
+    // console.log(thumb.scrollWidth);
+    document.addEventListener('mousemove', this._slider.moveHandler);
   }
 
-  mouseUp(event) {
+  _mouseUp(event) {
     console.log('up');
-    // document.removeEventListener('mousemove', this.mouseMoveHandler);
-    document.removeEventListener('mousemove', this.slider.moveHandler);
+
+    document.removeEventListener('mousemove', this._slider.moveHandler);
   }
 
-  mouseMove(event) {
-    console.log('mouseMove');
-    /*
-    if (((event.clientX - this.sliderLineCoords.left) > 0) &&((this.sliderLineCoords.right - this.sliderLineCoords.left) > event.clientX)) {
-      this.thumb.style.left = event.clientX - this.sliderLineCoords.left + 'px';
+  _mouseMove(event) {
+    const cursorX = event.clientX;
+    const sliderLeftGround = this._slider.line.getBoundingClientRect().left;
+    const thumbCenter  = this._slider.thumb.getBoundingClientRect().width / 2;
+
+    // value calculate
+    let value = cursorX - sliderLeftGround - thumbCenter;
+
+    this._setValue(value);
+  }
+
+  _setValue(value){
+    // limits calculate
+    const max = this._slider.line.getBoundingClientRect().width - this._slider.thumb.getBoundingClientRect().width;
+    const min = 0;
+
+    // move limits
+    if (value < min) {
+      // left limit
+      value = min
+    } else if (value > max) {
+      // right limit
+      value = max;
     }
-    */
+
+    this._slider.thumb.style.left = value + 'px';
+  }
+
+  setValue(value){
+    this._slider.thumb.style.left = value + 'px';
+  }
+
+  getValue(){
+    const lenght = this._slider.line.getBoundingClientRect().width - this._slider.thumb.getBoundingClientRect().width;
+    return this._slider.thumb.getBoundingClientRect().left
   }
 
 }
